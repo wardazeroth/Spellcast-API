5f2df65e2f7c9368cb87ef695610cbc530e04975
Javier Guarda <guardaajavier@gmail.com>
2025-07-29 23:20:02 -0400

    mapping orm from table reflect usuarios

--- app/routers/accounts.py
+++ app/routers/accounts.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 from fastapi.middleware import Middleware
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, registry
 from sqlalchemy import MetaData, Table, Column, Integer, String, select
 from sqlalchemy import create_engine
 from app.database import SessionLocal
@@ -9,10 +9,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 metadata= MetaData()
+mapper_registry = registry()
 
 engine = create_engine(
     DATABASE_URL
 )
+
+class Users:
+    pass
+
+tabla_usuarios= Table("users", metadata, autoload_with=engine)
+mapper_registry.map_imperatively(Users, tabla_usuarios)
 
 router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
@@ -24,18 +31,15 @@ def get_db():
         db.close()
         
 
-tabla_usuarios= Table("users", metadata, autoload_with=engine)
-
 
 @router.get('/')
 async def validar_token(request: Request, db: Session = Depends(get_db)): 
     user_id = request.state.user.get('id')
     print('el id del user es: ', user_id)
 
-    usuario_data = select(tabla_usuarios).where(tabla_usuarios.c.id == user_id)
-    usuario = db.execute(usuario_data).mappings().first()
-    print(db.execute(usuario_data))
-    
+    usuario= db.query(Users).filter(Users.id == user_id).first()
+    print(usuario)
+
     if not usuario:
         raise HTTPException(status_code=404, detail="User not found")
     
